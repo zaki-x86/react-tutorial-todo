a507ffd5ce14982e4e8d8828510f49c13297d000
zaki-x86 <mamadovzicx@gmail.com>
2023-08-07 09:33:57 +0200

    Updated <Todo />

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import Button from './Button';
 
-const Todo = ({task, id, completed}) => {
+const Todo = ({id, name, completed}) => {
     return <li>
         <div className="c-cb">
             <input id={"todo-"+id} type="checkbox" defaultChecked={completed} />
             <label className="todo-label" htmlFor={"todo-"+id}>
-            {id} - {task}
+            {id} - {name}
             </label>
       </div>
       <div className="btn-group">
         <button type="button" className="btn">
-        Edit <span className="visually-hidden">{task}</span>
+        Edit <span className="visually-hidden">{name}</span>
         </button>
         <button type="button" className="btn btn__danger">
-        Delete <span className="visually-hidden">{task}</span>
+        Delete <span className="visually-hidden">{name}</span>
         </button>
     </div>
     </li>
