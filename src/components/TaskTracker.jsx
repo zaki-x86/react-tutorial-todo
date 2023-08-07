72972370a77e5ec839aff44ffc880c1b7748c9e9
zaki-x86 <mamadovzicx@gmail.com>
2023-08-07 09:33:43 +0200

    Updated <TaskTracker />

--- src/components/TaskTracker.jsx
+++ src/components/TaskTracker.jsx
@@ -1,21 +1,24 @@
 const TaskTracker = () => {
-    return       <div className="filters btn-group stack-exception">
-    <button type="button" className="btn toggle-btn" aria-pressed="true">
-      <span className="visually-hidden">Show </span>
-      <span>all</span>
-      <span className="visually-hidden"> tasks</span>
-    </button>
-    <button type="button" className="btn toggle-btn" aria-pressed="false">
-      <span className="visually-hidden">Show </span>
-      <span>Active</span>
-      <span className="visually-hidden"> tasks</span>
-    </button>
-    <button type="button" className="btn toggle-btn" aria-pressed="false">
-      <span className="visually-hidden">Show </span>
-      <span>Completed</span>
-      <span className="visually-hidden"> tasks</span>
-    </button>
-  </div>
+    return <>
+      <div className="filters btn-group stack-exception">
+        <button type="button" className="btn toggle-btn" aria-pressed="true">
+          <span className="visually-hidden">Show </span>
+          <span>all</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+        <button type="button" className="btn toggle-btn" aria-pressed="false">
+          <span className="visually-hidden">Show </span>
+          <span>Active</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+        <button type="button" className="btn toggle-btn" aria-pressed="false">
+          <span className="visually-hidden">Show </span>
+          <span>Completed</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+    </div>
+    <h2 id="list-heading">3 tasks remaining</h2>
+  </>
 }
 
 export default TaskTracker;
