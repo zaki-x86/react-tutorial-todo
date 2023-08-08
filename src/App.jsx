aa0dcf8b3619b2c6f0bbe57d545fbef79f3d82fa
zaki-x86 <mamadovzicx@gmail.com>
2023-08-08 09:34:18 +0200

    Updated <App />

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,39 @@
-//import { useState } from 'react'
+import { useState } from 'react'
 import AddTodoForm from "./components/AddTodoForm";
 import Header from "./components/Header";
 import TaskList from './components/TaskList';
 import TaskTracker from "./components/TaskTracker";
-import Todo from "./components/Todo";
 
 const appName = "React ToDo";
 const appVersion = "v1.0";
 
+const addTask = (name) => {
+  // this function needs access to tasks object
+};
+
+const currentTasks = [
+  {
+    id: "123",
+    task: "Study",
+    completed: false
+  },
+  {
+    id: "153",
+    task: "Eat",
+    completed: true
+  }
+];
+
 function App() {
-const appInfo = {appName, appVersion};
+  // we need to pass `currentTasks` between 3 sibling components: AddTodoForm, TaskTracker, TaskList
+  const appInfo = {appName, appVersion};
 
   return (
     <div className='App'>
       <Header {...appInfo} />
-      <AddTodoForm />
+      <AddTodoForm addTask={addTask} />
       <TaskTracker />
-      <TaskList/>
+      <TaskList currentTasks={currentTasks} />
     </div>
   )
 }
